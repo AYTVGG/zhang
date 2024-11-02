c0dc58acf99751b8139cb4b73c8a0857633b8bbf
AYTVGG <zhangrui6811@qq.com>
2024-11-02 09:45:30 +0800

    Add files via upload

---
+++ Untitled1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#define PI 3.14
+int main ()
+{
+     float r, area;
+     /*
+     printf("ÇëÊäÈë°ë½ô:");
+     scanf("%f",&r);
+     area=PI*r*r;
+     printf("Ãæ¼¢Îª:%f.\n", area);
+     */
+     return 0;
+}
+
